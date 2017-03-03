fc7fd81d184f2a604d242cd5ebb3ee3d9c2e35d1
Hyce <jon.poullain@gmail.com>
2017-03-03 18:41:52 +0100

    ecran profil html

---
+++ profil.php
@@ -0,0 +1,71 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <title>Game</title>
+</head>
+<body><!-- Page Content -->
+<div class="back">
+    <div class="container profil">
+
+        <!-- Portfolio Item Heading -->
+        <div class="row">
+            <div class="col-lg-12" style="color: #ffffff;">
+                <h1 class="page-header">Profil
+                    <small>(Pseudo)</small>
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Item Row -->
+        <div class="row">
+
+            <div class="col-md-4">
+                <h3 style="color: #ffffff;">Infos Joueur</h3>
+
+                    <ul style="list-style: none;display: inline; color: #ffffff;">
+                    <li>Nom :</li>
+                    <li>Prénom :</li>
+                    <li>E-mail :</li>
+                    <li>Classe :</li>
+                </ul>
+            </div>
+
+            <div class="col-md-4" style="color: #ffffff;"><h3>Infos Personnage</h3>
+
+                <ul style="list-style: none;display: inline; color: #ffffff;">
+                    <li>Points de vie restant :</li>
+                    <li>Attaque :</li>
+                    <li>Défense :</li>
+                </ul>
+            </div>
+            <div class="col-md-4 amis">
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+        </footer>
+
+    </div>
+
+</div>
+<!-- /.container -->
+
+</body>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</html>
