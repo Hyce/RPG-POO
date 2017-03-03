9ed8b07950d5c7a4a73ba9c5bdb67f8c2d1199cd
Hyce <jon.poullain@gmail.com>
2017-03-03 18:10:09 +0100

    html pages

---
+++ defeat.php
@@ -0,0 +1,31 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <title>Game</title>
+</head>
+<body>
+<div class="back">
+    <div class="container">
+        <div class="col-md-6 col-md-offset-3 cen">
+            <div class="jumbotron">
+                <h1><i class="glyphicon glyphicon-hand-right"></i> Défaite <i class="glyphicon glyphicon-hand-left"></i></h1>
+                <p><i class="glyphicon glyphicon-circle-arrow-down"></i><i class="glyphicon glyphicon-circle-arrow-down"></i><i class="glyphicon glyphicon-circle-arrow-down"></i><i class="glyphicon glyphicon-circle-arrow-down"></i><i class="glyphicon glyphicon-circle-arrow-down"></i><i class="glyphicon glyphicon-circle-arrow-down"></i><i class="glyphicon glyphicon-circle-arrow-down"></i><i class="glyphicon glyphicon-circle-arrow-down"></i><i class="glyphicon glyphicon-circle-arrow-down"></i><i class="glyphicon glyphicon-circle-arrow-down"></i></p>
+                <a class="btn btn-success btn-lg" href="#" role="button">Recommencer   <i class="glyphicon glyphicon-repeat"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</html>
